617a30fbd5b2e60d8ca246b08a35b2751ac65909
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2022-04-10 21:13:57 +0200

    End section 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,22 @@ export class AppComponent {
   title = '12';
   data = 'hello code'
   num = 100;
+  displayVal: string ="";
 
   getVal() {
     return "Code step by step"
+  }
+
+  getName(name: string | number) {
+    alert(name);
+  }
+
+  getData(val: string) {
+    console.warn(val)
+  }
+
+  getValue(val: string) {
+    console.warn(val);
+    this.displayVal = val;
   }
 }
